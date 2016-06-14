7f7bb9ede52ca7f25c67b2fb5d39174e8b883dbf
neewy <n.yushkevich@hotmail.com>
2016-06-14 18:02:07 +0300

    Initial Commit

---
+++ InnoMaps/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  InnoMaps
+//
+//  Created by Nikolay Yushkevich on 14.06.16.
+//  Copyright © 2016 Nikolay Yushkevich. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
